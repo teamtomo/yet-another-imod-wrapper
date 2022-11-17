e8f78a1f42d220aff82b92cc5f3279d80a06a719
alisterburt <alisterburt@gmail.com>
2022-11-17 15:08:56 +0000

    add test for getting tilt angle offset (#21)

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def test_data_directory() -> Path:
+    return Path(__file__).parent / 'test_data'
+
+@pytest.fixture
+def align_log_file(test_data_directory) -> Path:
+    return test_data_directory / 'align.log'
